d2701e502af1f8c0decbdc20f5cc9b86b3f57e7e
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-03 11:57:13 +0500

    script.js
    varaible and condtion practice

---
+++ script.js
@@ -0,0 +1,74 @@
+// variabel create
+// var num=50;
+// var num1=10;
+// var reslt=num+num1;
+// console.log(reslt);
+// var a = 33;
+// var b = 10;
+// var c = "Test";
+// var result= a+b;
+// document.write("result",result,"<br/>");
+
+
+// var linebreak = "<br />";
+// document.write("a + b = ");
+// result = a + b;
+// document.write(result);
+// document.write(linebreak);
+// document.write(c);
+// var a = 33;
+// var b = 15;
+// var c = "Test";
+// // var result=a+b;
+// // document.write("a + b =",result)
+// var result=a%b;
+// document.write("result","=",result)
+
+// var a=50;
+// var b=10;
+// var c=50;
+// var d=30;
+// var e=35;
+
+// var reslt=a<b;
+// var reslt1=d<e;
+
+// document.write(reslt1)
+// var age=+prompt("USer enter age")
+// if (age>=18 && age<=30) {
+//     document.write("you are eligbale this post")
+// }
+// else{
+//     document.write("Your are not eligbal this post")
+// }
+// var age=15;
+// if (age >18) {
+//     document.write(true)
+
+// }
+// else{
+//     document.write(false)
+// }
+var book=prompt("User enter any book");
+if (book=="english") {
+    document.write("englsih book");
+}
+else if (book=="math") {
+    document.write("Math book");
+}
+else if (book=="urdu") {
+    document.write("Urdu book");
+}
+else if (book=="computer") {
+    
+    document.write("Computer book");
+
+}
+else if (book=="islamiyat") {
+    document.write("Islamiyat book");
+    
+}
+else{
+    document.write("other book");
+
+}
